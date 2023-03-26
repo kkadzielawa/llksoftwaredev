302f2a0281fe800b522a363f1cd1db3745f692a2
Konrad Kadzielawa <kkadzi25@gmail.com>
2023-03-26 17:37:28 -0500

    *added pagination

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .models import Post
 class BlogPageView(ListView):
     template_name = 'blog.html'
     model = Post
+    paginate_by = 2
     
 class BlogDetailView(DetailView):
     template_name = 'blog_detail.html'
