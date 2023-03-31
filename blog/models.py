05f093f200686fc2fde005fe103fe1d55fdbe56c
Konrad Kadzielawa <kkadzi25@gmail.com>
2023-03-31 16:14:52 -0500

    *adding comments section

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -13,3 +15,18 @@ class Post(models.Model):
     #associates each resource from blog detail with an absolute url
     def get_absolute_url(self):
         return reverse("blog_detail", kwargs={"slug": self.slug})
+
+
+class Comment(models.Model): # new
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        )
+    
+    def __str__(self):
+        return self.comment
+    
+    def get_absolute_url(self):
+        return reverse("post_list")
