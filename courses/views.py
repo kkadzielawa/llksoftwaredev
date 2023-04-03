92f82e95f267c58204866f3c1cad3ac482a567fa
Konrad Kadzielawa <kkadzi25@gmail.com>
2023-04-02 20:45:36 -0500

    *added stripe; *webhook in progress

--- courses/views.py
+++ courses/views.py
@@ -4,16 +4,51 @@ from django.http.response import JsonResponse # new
 from django.views.decorators.csrf import csrf_exempt # new
 from django.views.generic import TemplateView
 
+import stripe
 
 # Create your views here.
 
 class CoursesPageView(TemplateView):
     template_name = 'courses.html'
 
-print(settings.STRIPE_PUBLISHABLE_KEY)
-
 @csrf_exempt
 def stripe_config(request):
     if request.method == 'GET':
         stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
         return JsonResponse(stripe_config, safe=False)
+
+
+@csrf_exempt
+def create_checkout_session(request):
+    if request.method == 'GET':
+        domain_url = 'http://localhost:8000/courses/'
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        try:
+
+            # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+            checkout_session = stripe.checkout.Session.create(
+                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=domain_url + 'cancelled/',
+                payment_method_types=['card'],
+                mode='payment',
+                line_items=[
+                    {
+                        'price': 'price_1MsZZcCn7iEA5ilu8ey7bynw',
+                        #'name': 'Data Structures 101',
+                        'quantity': 1
+                        #'currency': 'usd'
+                    }
+                ]
+            )
+            print(checkout_session)
+            return JsonResponse({'sessionId': checkout_session['id']})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
+
+
+class SuccessView(TemplateView):
+    template_name = 'courses/success.html'
+
+
+class CancelledView(TemplateView):
+    template_name = 'courses/cancelled.html'
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,22 @@
 fetch("/courses/config/")
     .then((result) => { return result.json(); })
     .then((data) => {
+        // Initialize Stripe.js
         const stripe = Stripe(data.publicKey);
-        console.log(stripe)
+
+        // new
+        // Event handler
+        document.querySelector("#submitBtn").addEventListener("click", () => {
+            // Get Checkout Session ID
+            fetch("/courses/create-checkout-session/")
+                .then((result) => { return result.json(); })
+                .then((data) => {
+                    console.log(data);
+                    // Redirect to Stripe Checkout
+                    return stripe.redirectToCheckout({ sessionId: data.sessionId })
+                })
+                .then((res) => {
+                    console.log(res);
+                });
+        });
     });
